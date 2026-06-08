5380e110db9e87bb1428535b4a1e814743fc8943
Mohamed Magdy <mohamed9898magdy@gmail.com>
2026-06-08 15:51:16 +0300

    Add project files.

---
+++ Application/Behaviors/ValidatorBehavior.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+using MediatR;
+using System;
+using System.Text;
+
+namespace Application.Behaviors;
+
+public class ValidatorBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
+{
+    public IEnumerable<IValidator<TRequest>> validators;
+
+    public ValidatorBehavior(IEnumerable<IValidator<TRequest>> validators)
+    {
+        this.validators = validators;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if(validators.Any())
+        {
+            var context = new ValidationContext<TRequest>(request);
+
+            var validationResults = await Task.WhenAll(validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+
+            var failures = validationResults.SelectMany(r => r.Errors).Where(e => e is not null).ToList();
+
+            if (failures.Count != 0)
+                throw new ValidationException(failures);
+
+        }
+
+
+        return await next();
+    }
+}
---
+++ Domain/Todos/Todo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Todos;
+
+public class Todo
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public bool Completed { get; set; }
+}
